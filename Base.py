8a5c132cbb8aa1612d969e9d13319e6c0180feca
cybersj5 <163285309+cybersj5@users.noreply.github.com>
2024-04-11 18:07:04 +0300

    update_Base

--- Base.py
+++ Base.py
@@ -5,16 +5,21 @@ bot=telebot.TeleBot("7006292589:AAFikVQR1SSuXX5RsHxmWrYba3tgpHc265M")
 
 @bot.message_handler(comands=['start'])
 def start(message):
-    connection=sqlite3.connect("FIO.sql")
+    connection = sqlite3.connect("Users.db")
     curse = connection.cursor()
 
 
-    curse.execute('CREATE TABLE IF NOT EXISTS FIO ( id int auto_increment primary key, full_name varchar(100),snils varchar(11),exam_scores int)')
-    conn.comit()
+    curse.execute('CREATE TABLE IF NOT EXISTS '
+                  'Users'
+                  '(id int auto_increment primary key, '
+                  'full_name varchar(100),'
+                  'snils varchar(11),'
+                  'exam_scores int)')
+    connection.commit()
     curse.close
     connection.close
 
-    bot.send_message(massage.chat.id,"Данные внесены")
+    bot.send_message(message.chat.id,"Данные внесены")
 
 
 
